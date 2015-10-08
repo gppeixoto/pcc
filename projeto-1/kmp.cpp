0941f0c7fda7cedcff34be51d9f018e6c7455852
Rafael Acevedo <raa7@cin.ufpe.br>
2015-10-08 06:42:54 -0700

    Init project

---
+++ projeto-1/kmp.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+void preprocess(){
+
+
+}
+
+void kmp(string txt, string pat){
+	int n = txt.length();
+	int m = pat.length();
+	
+}
