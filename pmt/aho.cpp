53515fbcbc894deb64ff19acb58706ea815f0930
Guilherme Peixoto <gpp@cin.ufpe.br>
2015-10-15 00:28:43 -0300

    working optmized version of aho-corasick

--- pmt/aho.cpp
+++ pmt/aho.cpp
@@ -2,97 +2,87 @@
 #include <iostream>
 #include <assert.h>
 #include <string>
-#include <cstdio>
-#include <cstdlib>
 #include <queue>
-#include <algorithm>
-#include <map>
 #include <set>
 
 #define FAIL 0
 #define START 1
-#define ALPHABET_SIZE 26
-#define MAX_S 30
-#define DEBUG cout << "DEBUG" << endl
+#define MIN_CHAR 32
+#define MAX_CHAR 126
+#define SIZE MAX_CHAR - MIN_CHAR
+#define MAX_ST 30
 
 using namespace std;
 
-typedef std::vector<std::string> vs;
-typedef std::vector<int> vi;
+typedef vector<string> vs;
+typedef vector<int> vi;
+typedef pair<string, int> psi;
+typedef vector<psi> v_psi;
 
-int fail[MAX_S];
-int go_to[MAX_S][ALPHABET_SIZE];
-set<int> occ[MAX_S];
-
-void print_goto()
-{
-    cout << "  ";
-    for (int i=0; i < ALPHABET_SIZE; ++i) {
-        char tmp = i+'a';
-        cout << tmp << " ";
-    }
-    cout << endl;
-    for (int i=0; i < MAX_S; ++i) {
-        cout << i%10 << " ";
-        for (int j=0; j < ALPHABET_SIZE; ++j) {
-            cout << go_to[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-void print_fail()
-{
-    cout << "--FAIL--" << endl;
-    for (int i=0; i < MAX_S; ++i) {
-        cout << i << "\t" << fail[i] << endl;
-    }
-    cout << "--FAIL--" << endl;
-}
+int fail[MAX_ST];
+int go_to[MAX_ST][SIZE];
+set<string> occ[MAX_ST];
 
 void build_goto(const vs &patterns) {
-    memset(go_to, 0, sizeof go_to);
-    
-    int curr = START;
-    int state = 2;
-    for (int i=0; i < patterns.size(); ++i) {
-        curr = START;
-        string pat = patterns[i];
-        for (int j=0; j < pat.size(); ++j) {
-            int key = pat[j]-'a';
-            if (go_to[curr][key] == FAIL) {
-                go_to[curr][key] = state;
-                state += 1;
-            }
-            curr = go_to[curr][key];
+    memset(go_to, FAIL, sizeof go_to);
+    int newstate = START;
+    for (string pat : patterns)
+    {
+        int state = START;
+        int j = 0;
+        int pat_j = pat[j] - MIN_CHAR;
+
+        while (j < pat.length() && go_to[state][pat_j] != FAIL)
+        {
+            state = go_to[state][pat_j];
+            j += 1;
+            pat_j = pat[j] - MIN_CHAR;
         }
-        occ[curr].insert(i+1);
+
+        while (j < pat.length())
+        {
+            newstate += 1;
+            pat_j = pat[j] - MIN_CHAR;
+            go_to[state][pat_j] = newstate;
+            state = newstate;
+            j += 1;
+        }
+        occ[state].insert(pat);
     }
+
+    for (int a=0; a < SIZE; ++a)
+        if (go_to[START][a] == FAIL)
+            go_to[START][a] = START;
 }
 
 void build_fail(const vs &patterns) {
-
     queue<int> q;
-    for (int a=0; a < ALPHABET_SIZE; ++a) {
-        int s = go_to[START][a];
-        if (s != FAIL) {
-            q.push(s);
-            fail[s] = FAIL;
+    for (int a=0; a < SIZE; ++a)
+    {
+        if (go_to[START][a] != START) {
+            q.push(go_to[START][a]);
+            fail[go_to[START][a]] = START;
         }
     }
 
-    while (!q.empty()) {
-        int r = q.front(); q.pop();
-        for (int a=0; a < ALPHABET_SIZE; ++a) {
-            int s = go_to[r][a];
-            int u = r;
-            if (s != FAIL) {
-                q.push(s);
-                int state = fail[r];
-                while (u != FAIL && go_to[state][a] == FAIL)
-                    u = fail[u];
-                fail[u] = (u == START) ? START : go_to[fail[u]][a];
-                occ[s].insert(occ[fail[s]].begin(), occ[fail[s]].end());
+    while (!q.empty())
+    {
+        int cur = q.front(); q.pop();
+        for (int a=0; a < SIZE; ++a)
+        {
+            if (go_to[cur][a] != FAIL) {
+                int nextnode = go_to[cur][a];
+                q.push(go_to[cur][a]);
+                int brd = fail[cur];
+                while (go_to[brd][a] == FAIL)
+                    brd = fail[brd];
+                fail[nextnode] = go_to[brd][a];
+                set<string>::iterator it = occ[fail[nextnode]].begin();
+                while (it != occ[fail[nextnode]].end())
+                {
+                    occ[nextnode].insert(*it);
+                    ++it;
+                }
             }
         }
     }
@@ -103,30 +93,65 @@ void build_fsm(const vs &patterns) {
     build_fail(patterns);
 }
 
-void aho_corasick(const vs &patterns, const string &text){
-    int state = START;
-    for (int i=0; i < text.length(); ++i) {
-        int a_i = text[i]-'a';
-        while (go_to[state][a_i] == FAIL && state != FAIL)
-            state = fail[state];
-        state = (state == FAIL) ? START : go_to[state][a_i];
-        if (!occ[state].empty()) {
-            for (set<int>::iterator it = occ[state].begin();
-                    it != occ[state].end(); ++it)
-            {
-                int o = patterns[(*it)-1].length();
-                cout << i-o+1 << " " << patterns[(*it)-1] << endl;
-            }
+v_psi aho_corasick(const vs &patterns, const string &text) {
+    int cur = START;
+    v_psi occs;
+    for (int i=0; i < text.length(); ++i)
+    {
+        int a = text[i] - MIN_CHAR;
+        while (go_to[cur][a] == FAIL){
+            cur = fail[cur];
         }
+        cur = go_to[cur][a];
+        set<string>::iterator it = occ[cur].begin();
+        while (it != occ[cur].end())
+        {
+            occs.push_back(psi(*it, i-(*it).length()+1));
+            ++it;
+        }
+    }
+    return occs;
+}
+
+void print_results(const v_psi &occs, const string &text) {
+    for (int i=0; i < text.length(); ++i)
+    {
+        string prt = ((i/10)>0 && i%10==0) ? to_string(char(i/10)) : " ";
+        cout << prt;
+    }
+    cout << endl;
+
+    for (int i=0; i < text.length(); ++i)
+        cout << i%10;
+    
+    cout << endl;
+    cout << text << endl << endl;
+    for (psi pat_pos : occs)
+    {
+        cout << pat_pos.first << " " << pat_pos.second << endl;
     }
 }
 
 int main() {
     vs pats;
-    pats.push_back("she"); pats.push_back("som"); pats.push_back("he"); 
-    pats.push_back("hers");
+    pats.push_back("sit");
+    pats.push_back("sect");
+    pats.push_back("...");
+    pats.push_back("dolor");
+    pats.push_back(" dolor ");
+    pats.push_back("r s");
+    pats.push_back(",");
     build_fsm(pats);
-    string text = "guilhermepalmapeixoto";
-    aho_corasick(pats, text);
+
+    string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit...";
+    v_psi occs = aho_corasick(pats, text);
+
+    print_results(occs, text);
     return 0;
 }
+
+
+
+
+
+
