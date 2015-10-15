51076abe5cc1a7d2496d718f0a0e967a6804864d
Guilherme Peixoto <gpp@cin.ufpe.br>
2015-10-14 22:05:40 -0300

    performance improvement on aho-corasick

--- pmt/aho.cpp
+++ pmt/aho.cpp
@@ -7,132 +7,126 @@
 #include <queue>
 #include <algorithm>
 #include <map>
+#include <set>
 
-#define ALPHABET_SIZE 140
+#define FAIL 0
+#define START 1
+#define ALPHABET_SIZE 26
+#define MAX_S 30
+#define DEBUG cout << "DEBUG" << endl
 
 using namespace std;
 
-typedef pair<string, int> psi; 
+typedef std::vector<std::string> vs;
+typedef std::vector<int> vi;
 
-class Node {
-    public:
-        static int num;
-        int id;
-        map<char, Node*> go_to;
-        Node* fail;
-        vector<string> occ;
-        Node() : go_to(), fail(), occ(ALPHABET_SIZE), id(num){
-            num += 1;
-        };
-};
+int fail[MAX_S];
+int go_to[MAX_S][ALPHABET_SIZE];
+set<int> occ[MAX_S];
 
-int Node::num = 0;
-
-typedef pair<char, Node*> pcn;
-typedef map<char, Node*> map_pcn;
-
-Node* build_fail(Node* start){
-    queue<Node*> q;
-    Node* curr;
-    Node* next;
-    Node* brd;
-    for (int i=0; i < ALPHABET_SIZE; ++i){
-        if (start->go_to[i]->id != start->id){
-            q.push(start->go_to[i]);
-            start->go_to[i]->fail = start;
-        }
+void print_goto()
+{
+    cout << "  ";
+    for (int i=0; i < ALPHABET_SIZE; ++i) {
+        char tmp = i+'a';
+        cout << tmp << " ";
     }
-    while (!q.empty()){
-        curr = q.front(); q.pop();
-        map<char, Node*>::iterator it;
-        for (it = curr->go_to.begin(); it != curr->go_to.end(); ++it){
-            next = it->second; 
-            q.push(next);
-            brd = curr->fail;
-            char c = it->first;
-            map<char, Node*>::iterator it = brd->go_to.find(c);
-            
-            while (it == brd->go_to.end()){
-                brd = brd->fail;
-            }
-            next->fail = brd->go_to[c];
-            next->occ.insert(next->occ.end(), next->fail->occ.begin(), next->fail->occ.end());
+    cout << endl;
+    for (int i=0; i < MAX_S; ++i) {
+        cout << i%10 << " ";
+        for (int j=0; j < ALPHABET_SIZE; ++j) {
+            cout << go_to[i][j] << " ";
         }
+        cout << endl;
     }
-    return start;
 }
 
-Node* build_goto(Node* start, vector<string> patterns){
-    Node* curr;
-    Node* new_node;
-    int j;
-
-    for (int i=0; i < patterns.size(); ++i){
-        curr = start;
-        j = 0;
-        string pat = patterns[i];
-        map<char, Node*>::iterator it;  //looks for pat_0 in the
-        it = curr->go_to.find(pat[j]);  //adjacent nodes of start
-        while (j < pat.length() && it != curr->go_to.end()) {
-            curr = curr->go_to[pat[j]];
-            j += 1;
-            it = curr->go_to.find(pat[j]);
-        }
-
-        while (j < pat.length()){
-            new_node = new Node();
-            curr->go_to[pat[j]] = new_node;
-            curr = new_node;
-            j += 1;
-        }
-
-        curr->occ.push_back(pat);
-        for (int i=0; i < ALPHABET_SIZE; ++i){
-            char tmp = i;
-            map<char, Node*>::iterator it; it = start->go_to.find(tmp);
-            if (it == start->go_to.end()){ //if didnt find char i
-                start->go_to[tmp] = start;
-            }
-        }
+void print_fail()
+{
+    cout << "--FAIL--" << endl;
+    for (int i=0; i < MAX_S; ++i) {
+        cout << i << "\t" << fail[i] << endl;
     }
-    return start;
+    cout << "--FAIL--" << endl;
 }
 
-Node* build_fsm(vector<string> patterns){
-    Node* start = new Node();
-    start = build_goto(start, patterns);
-    start = build_fail(start);
-    return start;
-}
-
-vector<psi> aho_corasick(string text, vector<string> patterns){
-    Node* fsm = build_fsm(patterns);
-    Node* curr = fsm;
-    int len = text.length();
-    vector<psi> occurences;
-    map_pcn::iterator it;
+void build_goto(const vs &patterns) {
+    memset(go_to, 0, sizeof go_to);
     
-    for (int i=0; i<len; ++i){
-        it = curr->go_to.find(text[i]);
-        while (it == curr->go_to.end()){
-            curr = curr->fail;
-            it = curr->go_to.find(text[i]);
+    int curr = START;
+    int state = 2;
+    for (int i=0; i < patterns.size(); ++i) {
+        curr = START;
+        string pat = patterns[i];
+        for (int j=0; j < pat.size(); ++j) {
+            int key = pat[j]-'a';
+            if (go_to[curr][key] == FAIL) {
+                go_to[curr][key] = state;
+                state += 1;
+            }
+            curr = go_to[curr][key];
         }
-        curr = curr->go_to[text[i]];
-        for (int j=0; j<curr->occ.size(); ++j){
-            string pat = curr->occ[i];
-            occurences.push_back(psi(pat, i-pat.length()+1));
+        occ[curr].insert(i+1);
+    }
+}
+
+void build_fail(const vs &patterns) {
+
+    queue<int> q;
+    for (int a=0; a < ALPHABET_SIZE; ++a) {
+        int s = go_to[START][a];
+        if (s != FAIL) {
+            q.push(s);
+            fail[s] = FAIL;
         }
     }
-    return occurences;
+
+    while (!q.empty()) {
+        int r = q.front(); q.pop();
+        for (int a=0; a < ALPHABET_SIZE; ++a) {
+            int s = go_to[r][a];
+            int u = r;
+            if (s != FAIL) {
+                q.push(s);
+                int state = fail[r];
+                while (u != FAIL && go_to[state][a] == FAIL)
+                    u = fail[u];
+                fail[u] = (u == START) ? START : go_to[fail[u]][a];
+                occ[s].insert(occ[fail[s]].begin(), occ[fail[s]].end());
+            }
+        }
+    }
+}
+
+void build_fsm(const vs &patterns) {
+    build_goto(patterns);
+    build_fail(patterns);
+}
+
+void aho_corasick(const vs &patterns, const string &text){
+    int state = START;
+    for (int i=0; i < text.length(); ++i) {
+        int a_i = text[i]-'a';
+        while (go_to[state][a_i] == FAIL && state != FAIL)
+            state = fail[state];
+        state = (state == FAIL) ? START : go_to[state][a_i];
+        if (!occ[state].empty()) {
+            for (set<int>::iterator it = occ[state].begin();
+                    it != occ[state].end(); ++it)
+            {
+                int o = patterns[(*it)-1].length();
+                cout << i-o+1 << " " << patterns[(*it)-1] << endl;
+            }
+        }
+    }
 }
 
 int main() {
-    vector<string> pats;
-    pats.push_back("she"); pats.push_back("hers"); pats.push_back("he"); pats.push_back("ushers");
-    Node* start = build_fsm(pats);
-    string s = "he lost hers keys";
-    vector<psi> occ = aho_corasick(s, pats);
-
+    vs pats;
+    pats.push_back("she"); pats.push_back("som"); pats.push_back("he"); 
+    pats.push_back("hers");
+    build_fsm(pats);
+    string text = "guilhermepalmapeixoto";
+    aho_corasick(pats, text);
     return 0;
 }
